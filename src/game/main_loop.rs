bc448e4871f0dc5dd141c1335ce4d4fc2c79f166
Matej Strnad <matej.strnad.97@gmail.com>
2020-12-13 00:50:11 +0100

    Make user input framerate independent.

--- src/game/main_loop.rs
+++ src/game/main_loop.rs
@@ -3,13 +3,31 @@ use crate::Clients;
 use crate::Messages;
 use warp::ws::Message;
 
+const SPEED: isize = 30;
+
 pub async fn main_loop(room: &Room, clients: &Clients, messages: &Messages) {
-  // Handle messages
   let mut messages = messages.write().await;
   for (client_id, client_messages) in messages.clone().iter() {
-    for client_message in client_messages {
-      handle_message(client_id, client_message.clone(), clients, room).await;
+    if let Some(client) = clients.read().await.get(client_id) {
+      if let Some(player) = room.players.write().await.get_mut(&client.player_id) {
+        let old_position_x = player.position.0;
+        for client_message in client_messages {
+          player.handle_message(client_message);
+        }
+
+        // Lag position correction
+        let new_position_x = player.position.0;
+        let delta_x = new_position_x - old_position_x;
+        if delta_x.abs() > SPEED {
+          if delta_x > 0 {
+            player.position.0 = old_position_x + SPEED;
+          } else {
+            player.position.0 = old_position_x - SPEED;
+          }
+        }
+      }
     }
+
     messages.remove(client_id);
   }
 
@@ -17,30 +35,6 @@ pub async fn main_loop(room: &Room, clients: &Clients, messages: &Messages) {
   for client in clients.read().await.values() {
     if let Some(sender) = &client.sender {
       let _ = sender.send(Ok(Message::text(room.to_datastring().await)));
-    }
-  }
-}
-
-async fn handle_message(id: &str, msg: Message, clients: &Clients, room: &Room) {
-  let message = match std::str::from_utf8(msg.as_bytes()) {
-    Ok(value) => value,
-    Err(_) => return,
-  };
-
-  let message: Vec<&str> = message.split("/").collect();
-  if message.len() != 2 {
-    println!("Bad message");
-    return;
-  }
-
-  let message_name = message[0];
-  let message_payload = message[1];
-
-  if let Some(client) = clients.read().await.get(id).cloned() {
-    let mut locked = room.players.write().await;
-
-    if let Some(player) = locked.get_mut(&client.player_id) {
-      player.handle_message(message_name, message_payload)
     }
   }
 }
--- src/game/player.rs
+++ src/game/player.rs
@@ -1,9 +1,11 @@
+use warp::ws::Message;
+
 #[derive(Debug, Clone)]
 pub struct Player {
   id: String,
   client_id: String,
   name: String,
-  position: (isize, isize),
+  pub position: (isize, isize),
   last_input_sequence: isize,
 }
 
@@ -18,33 +20,46 @@ impl Player {
     }
   }
 
-  pub fn handle_message(&mut self, name: &str, payload: &str) {
+  pub fn handle_message(&mut self, message: &Message) {
+    let message = match std::str::from_utf8(message.as_bytes()) {
+      Ok(value) => value,
+      Err(_) => return,
+    };
+    let message: Vec<&str> = message.split("/").collect();
+    if message.len() != 2 {
+      println!("Bad message");
+      return;
+    }
+    let name = message[0];
+    let payload = message[1];
+
     if name != "move" {
       return;
     };
 
     let payload: Vec<&str> = payload.split(";").collect();
-    if payload.len() != 2 {
+    if payload.len() != 3 {
       println!("Bad payload");
       return;
     }
 
-    let direction = payload[0];
-    let input_sequence = payload[1];
+    let axis = payload[0];
+    let value = payload[1];
+    let input_sequence = payload[2];
 
-    match direction {
-      "right" => self.position.0 += 10,
-      "left" => self.position.0 -= 10,
-      "down" => self.position.1 += 10,
-      "up" => self.position.1 -= 10,
-      _ => {
-        println!("Bad direction: {}", direction);
-        return;
+    if let Ok(value) = value.parse::<isize>() {
+      match axis {
+        "x" => self.position.0 += value,
+        "y" => self.position.1 += value,
+        _ => {
+          println!("Invalid axis: {}", axis);
+          return;
+        }
       }
-    }
 
-    if let Ok(input_sequence) = input_sequence.parse::<isize>() {
-      self.last_input_sequence = input_sequence;
+      if let Ok(input_sequence) = input_sequence.parse::<isize>() {
+        self.last_input_sequence = input_sequence;
+      }
     }
   }
 
